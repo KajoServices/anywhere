05f9607a48e5c7c49085843ec76d6c6b07b9a8c9
Denis Kolokol <dkolokol@gmail.com>
2018-08-03 14:52:33 +0200

    corret imports

--- celerytasks.py
+++ celerytasks.py
@@ -1,8 +1,11 @@
-import datetime
-import geopy
-import logging
+import os
+import sys
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+import datetime
+import logging
+import geopy
 
 from django.conf import settings
 from django.utils import timezone
